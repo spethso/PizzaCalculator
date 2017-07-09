31cc07fa10fcd220c39c7c43279cc96f876ffdc9
spethso <spethso@studi.informatik.uni-stuttgart.de>
2017-07-09 02:08:58 +0200

    Changed 40X status code response

--- src/index.js
+++ src/index.js
@@ -40,7 +40,8 @@ app.get('/pizzas/amount', function (req, res) {
         };
         res.status(200).end(JSON.stringify(amount));
     } else {
-        res.sendStatus(400);
+        // res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 });
 
@@ -48,7 +49,7 @@ app.get('/pizzas/ingredients', function (req, res) {
     if (ingredients.length > 0) {
         res.status(200).end(JSON.stringify(ingredients));
     } else {
-        res.sendStatus(404);
+        res.sendStatus(404).end('Not Found');
     }
 });
 
@@ -58,7 +59,7 @@ app.get('/pizzas/suggestions', function (req, res) {
         let suggestions = team_suggestions.get(teamname);
         res.status(200).end(JSON.stringify(suggestions));
     } else {
-        res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 });
 
@@ -72,7 +73,7 @@ app.get('/teamdata', function (req, res) {
     if (teamname != undefined && teams.has(teamname)) {
         res.status(200).end(JSON.stringify(teams.get(teamname)));
     } else {
-        res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 })
 
@@ -94,7 +95,7 @@ app.post('/sessioncreate/', function (req, res) {
         res.status(200).end(JSON.stringify({ teamname: teamname }));
     } else {
         console.log('Creating Team: Failure because of undefined teamname!');
-        res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 });
 
@@ -118,7 +119,7 @@ app.post('/teams/teamsize/', function (req, res) {
         res.status(200).end(JSON.stringify({ teamname: teamname, data: data }));
     } else {
         console.log('Update team size failed');
-        res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 });
 
@@ -130,13 +131,13 @@ app.post('/pizzas/suggestions', function (req, res) {
         let suggestion = {
             id: team_suggestions.get(teamname).length,
             ingredients: ingredients,
-            vote: 1
+            vote: 0
         };
         team_suggestions.get(teamname).push(suggestion);
         res.end(JSON.stringify(suggestion));
     } else {
         console.log('Failed to add suggestion');
-        res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 });
 
@@ -151,7 +152,7 @@ app.post('/pizzas/suggestions/vote', function (req, res) {
         res.sendStatus(200);
     } else {
         console.log('Update voting failed');
-        res.sendStatus(400);
+        res.sendStatus(400).end('Bad request');
     }
 });
 
