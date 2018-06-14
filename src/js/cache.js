3ccfd23affd51e0ad0df3574ab7a226526cd768e
Matthias Hermann <matthias.h@online.de>
2018-06-14 12:43:34 +0200

    Add GDPR Footer on index page

--- src/js/cache.js
+++ src/js/cache.js
@@ -3,5 +3,15 @@ window.addEventListener('load', () => {
         .submit(() => {
             window.localStorage.clear();
             console.log("Local storage has been cleared")
-        })
+        });
+
+    // Add GDPR footer
+    $.ajax({
+        url: './application/gdpr',
+        success: (res) => {
+            $('#gdpr-footer')
+                .html(res)
+        },
+        method: "GET",
+    })
 })
